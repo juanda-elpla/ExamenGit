05f9f54bc855f287bca2c3001e02a6f2a603876e
EDU\denbar3 <denbar3@alu.edu.gva.es>
2024-01-18 16:30:03 +0100

    subo-la-correcion

--- src/main/java/com/mycompany/examengit/ExamenGit.java
+++ src/main/java/com/mycompany/examengit/ExamenGit.java
@@ -11,6 +11,7 @@ package com.mycompany.examengit;
 public class ExamenGit {
 
     public static void main(String[] args) {
-        System.out.println("Esta línea tiene un fallo. Encuéntralo");
+        System.out.println("Esta línea tiene un fallo. Encuentralo, no me sale el error!!!");
+        
     }
 }
