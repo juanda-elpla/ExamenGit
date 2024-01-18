aaec054921d3a4ea8045c9b5b7d608339e25d6a7
JuanDaniel <juandablasco@gmail.com>
2024-01-18 13:22:21 +0100

    Primer commit del proyecto

---
+++ src/main/java/com/mycompany/examengit/ExamenGit.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.examengit;
+
+/**
+ *
+ * @author Usuario
+ */
+public class ExamenGit {
+
+    public static void main(String[] args) {
+        System.out.println("Esta línea tiene un fallo. Encuéntralo")
+    }
+}
